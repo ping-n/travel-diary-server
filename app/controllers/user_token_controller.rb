1bf210f5ba6d4a0c4d4468bfd6085eee61bab987
PING NGE <ping.nge@hotmail.com>
2020-07-09 22:38:18 +1000

    generated user model and controller

---
+++ app/controllers/user_token_controller.rb
@@ -0,0 +1,2 @@
+class UserTokenController < ApplicationController
+end
---
+++ test/controllers/user_token_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserTokenControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
