2257c62b8bdbed8a60eea5550aa58f68664ad169
PING NGE <ping.nge@hotmail.com>
2020-07-09 22:46:22 +1000

    added route for login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def create
+    User.create(user_params)
+    render json: "user created", status: 200
+  end
+
+  private 
+  def user_params 
+    params.require(:user).permit(:email, :password)
+  end 
 end
