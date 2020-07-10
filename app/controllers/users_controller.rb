6233fda1665681c94b0aca04fe377e16e6643dfa
PING NGE <ping.nge@hotmail.com>
2020-07-10 13:42:46 +1000

    reverted back to simple routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,7 @@
 class UsersController < ApplicationController
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: "user created", status: :created
-    else
-      render json: user.errors.full_messages, status: :failed
-    end
-  end
-
-  def update 
-    if user_params[:email] == ''
-      current_user.password = user_params[:password]
-    else
-      current_user.email = user_params[:email]
-    end
-    render json: current_user.email, status: :ok if current_user.save
+    User.create(user_params)
+    render json: "user created", status: 200
   end
 
   private 
